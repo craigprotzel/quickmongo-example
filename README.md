fb52e72a02dfd0461bafd9c4297a54b3966763fe
Mathura MG <mmg9291@gmail.com>
2022-10-04 11:02:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Steps followed
+
+* Install home-brew
+* https://brew.sh/
+* `brew tap mongodb/brew
+* `brew update `
+* `brew install mongodb-community@6.0`
+
+* `brew services start mongodb-community@6.0`
+* `brew services stop mongodb-community@6.0`
+
---
+++ index.js
@@ -0,0 +1,50 @@
+let {Database} = require("quickmongo");
+
+const db = new Database("mongodb://localhost:27017/quickmongo");
+
+db.on("ready", () => {
+    console.log("Connected to the database");
+    doStuff();
+});
+
+// top-level awaits
+db.connect(); 
+
+async function doStuff() {
+    // Setting an object in the database:
+    await db.set("userInfo", { difficulty: "Easy" });
+    // -> { difficulty: 'Easy' }
+
+    // Pushing an element to an array (that doesn't exist yet) in an object:
+    await db.push("userInfo.items", "Sword");
+    // -> { difficulty: 'Easy', items: ['Sword'] }
+
+    // Adding to a number (that doesn't exist yet) in an object:
+    await db.add("userInfo.balance", 500);
+    // -> { difficulty: 'Easy', items: ['Sword'], balance: 500 }
+
+    // Repeating previous examples:
+    await db.push("userInfo.items", "Watch");
+    // -> { difficulty: 'Easy', items: ['Sword', 'Watch'], balance: 500 }
+    await db.add("userInfo.balance", 500);
+    // -> { difficulty: 'Easy', items: ['Sword', 'Watch'], balance: 1000 }
+
+    // Fetching individual properties
+    await db.get("userInfo.balance"); // -> 1000
+    await db.get("userInfo.items"); // -> ['Sword', 'Watch']
+
+    // remove item
+    await db.pull("userInfo.items", "Sword");
+    // -> { difficulty: 'Easy', items: ['Watch'], balance: 1000 }
+
+    // set the data and automatically delete it after 1 minute
+    await db.set("foo", "bar", 60); // 60 seconds = 1 minute
+
+    // fetch the temporary data after a minute
+    setTimeout(async () => {
+       let test = await db.get("userInfo.items");
+       console.log(test);
+
+        // await db.get("foo"); // null
+    }, 1000);
+}
